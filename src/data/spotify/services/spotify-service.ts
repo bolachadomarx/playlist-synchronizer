2863f091a81ff4bed5c67d9cd11e07f8a890005b
Joao Guilherme <tertuliano@sunnit.com.br>
2020-11-01 16:27:38 -0300

    feat: prepare authorize service, to get client access token

--- src/data/spotify/services/spotify-service.ts
+++ src/data/spotify/services/spotify-service.ts
@@ -1,13 +1,26 @@
-import { AuthorizationModel, AccountAccessData } from '@/domain/spotify/models/authorization'
+import { AuthorizationModel } from '@/domain/spotify/models/authorization'
 import { GetAccessToken } from '@/domain/spotify/usecases/get-access-token'
 import { StoreAccessCode } from '@/domain/spotify/usecases/store-access-code'
-
+import axios from 'axios'
+import qs from 'qs'
 export class SpotifyService implements StoreAccessCode, GetAccessToken {
+  spotifyApi = 'https://accounts.spotify.com/api'
+
   async store(accessCode: string): Promise<string> {
     return await accessCode
   }
 
-  authorize(authorizationData: AuthorizationModel): Promise<AccountAccessData> {
-    return new Promise((resolve) => false)
+  async authorize(authorizationData: AuthorizationModel): Promise<any> {
+    const { grant_type, redirect_uri, code, basic } = authorizationData
+    const body = { grant_type, redirect_uri, code }
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: basic,
+      },
+    }
+    const response = await axios.post(`${this.spotifyApi}/token`, qs.stringify(body), config)
+    const accountAccess = response.data
+    return accountAccess
   }
 }
