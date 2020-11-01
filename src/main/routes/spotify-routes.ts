dfb357c196cfcc0c7f08a039d8519d4fb5ac392e
Joao Guilherme <tertuliano@sunnit.com.br>
2020-11-01 16:59:21 -0300

    fix: make authorization a single route

--- src/main/routes/spotify-routes.ts
+++ src/main/routes/spotify-routes.ts
@@ -1,9 +1,7 @@
-import { makeGetAccessToken } from './../factories/spotify/get-access-token-factory'
-import { makeStoreAccessCode } from './../factories/spotify/store-acces-code-factory'
+import { makeAuthorization } from './../factories/spotify/authorization-factory'
 import { adaptRoute } from './../adapters/express-route-adapter'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.get('/access-code', adaptRoute(makeStoreAccessCode()))
-  router.post('/authorization', adaptRoute(makeGetAccessToken()))
+  router.get('/access-code', adaptRoute(makeAuthorization()))
 }
